06fc5f408b73d77b0ad0d7177d5edc71f28ed7d6
hansan <hansan9611@gmail.com>
2023-11-14 17:04:29 +0900

    Vue.js Axios실습

--- Ch10/src/main/java/kr/ch10/controller/User1Controller.java
+++ Ch10/src/main/java/kr/ch10/controller/User1Controller.java
@@ -4,55 +4,64 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import kr.ch10.entity.User1Entity;
 import kr.ch10.service.User1Service;
+import lombok.extern.log4j.Log4j2;
 
+@Log4j2
+@CrossOrigin("http://localhost:5173")
 @Controller
 public class User1Controller {
 
 	@Autowired
 	private User1Service service;
 	
-	
-	@ResponseBody // json 데이터로 클라이언트에게 응답 / ajax로 요청하고 json으로 응답하므로 다 메서드마다 선언해줘야 함
+	/*
+	 * CORS(Cross-Origin Resource Sharing Policy)
+	 *  - HTTP 웹 서비스 보안정책 중 하나로 동일 출처의 리소스만 공유하는 원칙(SOP)
+	 *  - @CrossOrigin 어노테이션으로 특정 Origin 주소를 설정하면 해당 Origin에서만 응답처리
+	 */
+	@ResponseBody
 	@GetMapping("/user1")
 	public List<User1Entity> list() {
-		
 		List<User1Entity> user1s = service.selectUser1s();
-		
 		return user1s;
 	}
 	
 	@ResponseBody
 	@GetMapping("/user1/{id}")
 	public User1Entity user1(@PathVariable("id") String id) {
-		
 		return service.selectUser1(id);
-		
 	}
 	
 	@ResponseBody
 	@PostMapping("/user1")
-	public void register(User1Entity user1) {
+	public void register(@RequestBody User1Entity user1) {		
+		log.info(user1);		
 		service.insertUser1(user1);
+		
 	}
 	
 	@ResponseBody
 	@PutMapping("/user1")
-	public void modify(User1Entity user1) {
+	public void modify(@RequestBody User1Entity user1) {
+		
+		log.info(user1);	
 		service.updateUser1(user1);
 	}
 	
 	@ResponseBody
 	@DeleteMapping("/user1/{id}")
-	public void delete(@PathVariable("id") String id) { 
+	public void delete(@PathVariable("id") String id) {
 		service.deleteUser1(id);
 	}
 }
